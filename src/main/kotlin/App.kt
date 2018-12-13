cdbb2e64559fbff438b14e692fdb1d31e4288f84
BaiJiFeiLong <baijifeilong@gmail.com>
2018-12-14 00:08:29 +0800

    Add progress bar

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,16 +1,22 @@
 import javafx.application.Application
 import javafx.collections.ObservableList
 import javafx.scene.control.Label
+import javafx.scene.control.ProgressBar
+import javafx.scene.control.Slider
+import javafx.scene.layout.Priority
+import javafx.scene.paint.Color
 import org.apache.commons.io.FileUtils
 import org.apache.commons.io.FilenameUtils
 import tornadofx.*
 import uk.co.caprica.vlcj.component.AudioMediaPlayerComponent
 import uk.co.caprica.vlcj.player.MediaPlayer
+import uk.co.caprica.vlcj.player.MediaPlayerEventAdapter
 import java.io.File
 import java.nio.charset.Charset
 import java.nio.file.FileSystems
 import java.nio.file.Files
 import java.nio.file.Paths
+import kotlin.math.abs
 import kotlin.streams.toList
 
 class Music(artist: String, title: String, filename: String) {
@@ -27,28 +33,74 @@ class MainView : View() {
     private val mainController: MainController by inject()
     private val musics = mainController.loadMusics()
     private val player = mainController.loadPlayer()
+    private lateinit var slider: Slider
+    private lateinit var progressBar: ProgressBar
     private lateinit var musicLabel: Label
 
-    override val root = hbox {
-        tableview(musics) {
+    override val root = vbox {
+        hbox {
             minWidth = 800.0
-            minHeight = 600.0
-            column("Artist", Music::artist) {
-                maxWidth = 200.0
+            tableview(musics) {
+                hgrow = Priority.ALWAYS
+                column("Artist", Music::artist) {
+                    hgrow = Priority.NEVER
+                    maxWidth = 200.0
+                }
+                column("Title", Music::title) {
+                    hgrow = Priority.ALWAYS
+                }
+                onUserSelect {
+                    player.playMedia(it.filename)
+                    player.addMediaPlayerEventListener(object : MediaPlayerEventAdapter() {
+                        override fun paused(mediaPlayer: MediaPlayer?) {
+                            super.paused(mediaPlayer)
+                        }
+
+                        override fun positionChanged(mediaPlayer: MediaPlayer?, newPosition: Float) {
+                            super.positionChanged(mediaPlayer, newPosition)
+                            slider.value = newPosition.toDouble()
+                            println("Position: $newPosition")
+                        }
+                    })
+                    val lrcFilename = it.filename.replace(Regex("\\.[^.]+$"), ".lrc")
+                    println(lrcFilename)
+                    val lrcText = FileUtils.readFileToString(File(lrcFilename), Charset.forName("GBK"))
+                    println(lrcText)
+                    musicLabel.text = lrcText
+                }
             }
-            column("Title", Music::title)
-            onUserSelect {
-                player.playMedia(it.filename)
-                val lrcFilename = it.filename.replace(Regex("\\.[^.]+$"), ".lrc")
-                println(lrcFilename)
-                val lrcText = FileUtils.readFileToString(File(lrcFilename), Charset.forName("GBK"))
-                println(lrcText)
-                musicLabel.text = lrcText
+            label("Lyric show") {
+                hgrow = Priority.ALWAYS
+                musicLabel = this
+            }
+            button("Click me") {
+                setOnMouseClicked {
+                    player.setPause(player.isPlaying)
+                }
             }
         }
-        label("Lyric show") {
-            musicLabel = this
-            minWidth = 400.0
+
+        stackpane {
+            progressbar(initialValue = .0) {
+                progressBar = this
+                maxWidth = Double.MAX_VALUE
+            }
+            slider(max = 1) {
+                slider = this
+                maxWidth = Double.MAX_VALUE
+            }
+        }
+    }
+
+    init {
+        progressBar.progressProperty().bind(slider.valueProperty())
+        progressBar.paddingLeftProperty.bind(progressBar.heightProperty().divide(2))
+        progressBar.paddingRightProperty.bind(progressBar.heightProperty().divide(2))
+
+        slider.valueProperty().addListener { observable, oldValue, newValue ->
+            if (abs(newValue.toDouble() - oldValue.toDouble()) * player.length > 999) {
+                player.position = newValue.toFloat()
+            }
         }
     }
 }
@@ -58,6 +110,11 @@ class MainStylesheet : Stylesheet() {
         root {
             fontFamily = "Noto Sans CJK SC Medium"
             fontSize = 20.px
+            slider {
+                track {
+                    backgroundColor = MultiValue(arrayOf(Color.TRANSPARENT))
+                }
+            }
         }
     }
 }
